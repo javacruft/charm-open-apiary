51697eeb3aa675bc302cf198259942599fd40e6b
James Page <james.page@ubuntu.com>
2021-05-25 09:22:03 +0100

    Remove more template boilerplate

--- src/charm.py
+++ src/charm.py
@@ -1,16 +1,6 @@
 #!/usr/bin/env python3
 # Copyright 2021 James Page
 # See LICENSE file for licensing details.
-#
-# Learn more at: https://juju.is/docs/sdk
-
-"""Charm the service.
-
-Refer to the following post for a quick-start guide that will help you
-develop a new k8s charm using the Operator Framework:
-
-    https://discourse.charmhub.io/t/4208
-"""
 
 import json
 import logging

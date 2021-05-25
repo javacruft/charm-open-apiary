6c0dd05846bb8c59c692693f754763d6179d1798
James Page <james.page@ubuntu.com>
2021-05-25 10:30:14 +0100

    Misc updates for unit testing

--- tests/test_charm.py
+++ tests/test_charm.py
@@ -33,12 +33,15 @@ class TestCharm(unittest.TestCase):
         self.harness = Harness(OpenApiaryCharm)
         self.addCleanup(self.harness.cleanup)
         self.harness.begin()
+        # NOTE(jamespage)
+        # Mock out push and pull as not implemented in test harness
+        container = self.harness.model.unit.get_container("open-apiary")
+        container.push = MagicMock()
+        container.pull = MagicMock()
+        container.pull.return_value = io.StringIO(NODE_VERSION_INFO)
         self.maxDiff = None
 
-    def test_config_changed(self):
-        # Check the initial Pebble plan is empty
-        initial_plan = self.harness.get_container_pebble_plan("open-apiary")
-        self.assertEqual(initial_plan.to_yaml(), "{}\n")
+    def _test_config_changed(self, weather_token=""):
         # Expected plan with default config
         expected_plan = {
             "services": {
@@ -53,7 +56,7 @@ class TestCharm(unittest.TestCase):
                         "UPLOAD_PATH": "/uploads",
                         "LOG_DESTINATION": "/data/open-apiary.log",
                         "LOG_LEVEL": "info",
-                        "WEATHER_API_KEY": "",
+                        "WEATHER_API_KEY": weather_token,
                     },
                 }
             }
@@ -61,19 +64,10 @@ class TestCharm(unittest.TestCase):
 
         # Get the open-apiary container from the model
         container = self.harness.model.unit.get_container("open-apiary")
-        # NOTE(jamespage)
-        # Mock out push and pull as not implemented in test harness
-        container = self.harness.model.unit.get_container("open-apiary")
-        container.push = MagicMock()
-        container.pull = MagicMock()
-        container.pull.return_value = io.StringIO(NODE_VERSION_INFO)
-
-        self.harness.charm.on.config_changed.emit()
+        self.harness.update_config({"weather-api-token": weather_token or None})
         # Everything happens on config-changed so just emit this event
         # Get the plan now we've run PebbleReady
-        updated_plan = self.harness.get_container_pebble_plan(
-            "open-apiary"
-        ).to_dict()
+        updated_plan = self.harness.get_container_pebble_plan("open-apiary").to_dict()
         # Check we've got the plan we expected
         self.assertEqual(expected_plan, updated_plan)
         # Check configuration file pushed to container
@@ -94,43 +88,8 @@ class TestCharm(unittest.TestCase):
         self.assertEqual(self.harness.model.unit.status, ActiveStatus())
         self.assertEqual(self.harness.get_workload_version(), "1.1.1")
 
-        # Reset Mocks ready for next execution of config-changed
-        container.push.reset_mock()
-        container.pull.reset_mock()
-        container.pull.return_value = io.StringIO(NODE_VERSION_INFO)
+    def test_config_changed(self):
+        self._test_config_changed()
 
-        self.harness.update_config({"weather-api-token": "mytoken"})
-        updated_plan = self.harness.get_container_pebble_plan(
-            "open-apiary"
-        ).to_dict()
-        expected_plan = {
-            "services": {
-                "open-apiary": {
-                    "override": "replace",
-                    "summary": "open-apiary",
-                    "command": "/usr/local/bin/npm start",
-                    "startup": "enabled",
-                    "environment": {
-                        "PORT": "3000",
-                        "DATA_PATH": "/data",
-                        "UPLOAD_PATH": "/uploads",
-                        "LOG_DESTINATION": "/data/open-apiary.log",
-                        "LOG_LEVEL": "info",
-                        "WEATHER_API_KEY": "mytoken",
-                    },
-                }
-            }
-        }
-        self.assertEqual(expected_plan, updated_plan)
-        container.push.assert_called_once_with(
-            "/opt/app/config.json",
-            json.dumps(
-                self.harness.charm._open_apiary_config(),
-                sort_keys=True,
-                indent=2,
-            ),
-            make_dirs=True,
-        )
-        self.assertTrue(service.is_running())
-        self.assertEqual(self.harness.model.unit.status, ActiveStatus())
-        self.assertEqual(self.harness.get_workload_version(), "1.1.1")
+    def test_config_changed_weather_token_set(self):
+        self._test_config_changed(weather_token="mytoken")
